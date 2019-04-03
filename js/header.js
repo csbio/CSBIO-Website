8548f2e1e53fddc83db42acc6ceff5f7d5b71437
Benjamin VanderSluis <ben@bjv.us>
2019-04-03 15:39:40 -0500

    relative links, removed pagination
    Pages had a pagination section but only one page of content.
    I also make more links relative, so that you can debug changes while
    pointing your browser at a local directory

--- js/header.js
+++ js/header.js
@@ -9,32 +9,32 @@ document.write(
 "                    <span class='icon-bar'></span>" +
 "                    <span class='icon-bar'></span>" +
 "                </button>" +
-"                <a class='navbar-brand' href='/index.html'>Home</a>" +
+"                <a class='navbar-brand' href='index.html'>Home</a>" +
 "            </div>" +
 "            <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>" +
 "                <ul class='nav navbar-nav navbar-right'>" +
 "                    <li>" +
-"                        <a href='/research.html'>Research</a>" +
+"                        <a href='research.html'>Research</a>" +
 "                    </li>" +
 "                    <li>" +
-"                        <a href='/news.html'>News</a>" +
+"                        <a href='news.html'>News</a>" +
 "                    </li>" +
 "                    <li>" +
-"                        <a href='/people.html'>People</a>" +
+"                        <a href='people.html'>People</a>" +
 "                    </li>" +
 "                    <li>" +
-"                        <a href='/publication.html'>Publications</a>" +
+"                        <a href='publication.html'>Publications</a>" +
 "                    </li>" +
 "                    <li>" +
-"                        <a href='/software.html'>Software</a>" +
+"                        <a href='software.html'>Software</a>" +
 "                    </li>" +
 "                    <li class='dropdown'>" +
 "                        <a href='#' class='dropdown-toggle' data-toggle='dropdown'>Other Pages <b class='caret'></b></a>" +
 "                        <ul class='dropdown-menu'>" +
-"                            <li><a href='/LabMemberArea/LabMemberArea.html'>Lab Member Area</a></li>" +
-"                            <li><a href='/collaborators.html'>Collaborators</a></li>" +
-"                            <li><a href='/positions.html'>Positions</a></li>" +
-"                            <li><a href='/contact.html'>Contact us</a></li>" +
+"                            <li><a href='LabMemberArea/LabMemberArea.html'>Lab Member Area</a></li>" +
+"                            <li><a href='collaborators.html'>Collaborators</a></li>" +
+"                            <li><a href='positions.html'>Positions</a></li>" +
+"                            <li><a href='contact.html'>Contact us</a></li>" +
 "                        </ul>" +
 "                    </li>" +
 "                </ul>" +
