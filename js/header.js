d303929d13bff8e8557a8b509d421130c68c7cd2
Henry Ward <wardx596@umn.edu>
2019-10-10 15:09:48 -0500

    Started migration to bootstrap 4

--- js/header.js
+++ js/header.js
@@ -1,44 +1,37 @@
 //This is the header for every web page so we don't have to copy paste it
 
-var html =  "    <nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>" +
-            "        <div class='container'>" +
-            "            <div class='navbar-header'>" +
-            "                <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>" +
-            "                    <span class='sr-only'>Toggle navigation</span>" +
-            "                    <span class='icon-bar'></span>" +
-            "                    <span class='icon-bar'></span>" +
-            "                    <span class='icon-bar'></span>" +
-            "                </button>" +
-            "                <a class='navbar-brand' href='/index.html'>Home</a>" +
-            "            </div>" +
-            "            <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>" +
-            "                <ul class='nav navbar-nav navbar-right'>" +
-            "                    <li>" +
-            "                        <a href='/research.html'>Research</a>" +
-            "                    </li>" +
-            "                    <li>" +
-            "                        <a href='/news.html'>News</a>" +
-            "                    </li>" +
-            "                    <li>" +
-            "                        <a href='/people.html'>People</a>" +
-            "                    </li>" +
-            "                    <li>" +
-            "                        <a href='/publication.html'>Publications</a>" +
-            "                    </li>" +
-            "                    <li>" +
-            "                        <a href='/software.html'>Software</a>" +
-            "                    </li>" +
-            "                    <li class='dropdown'>" +
-            "                        <a href='#' class='dropdown-toggle' data-toggle='dropdown'>Other Pages <b class='caret'></b></a>" +
-            "                        <ul class='dropdown-menu'>" +
-            "                            <li><a href='/LabMemberArea/LabMemberArea.html'>Lab Member Area</a></li>" +
-            "                            <li><a href='/collaborators.html'>Collaborators</a></li>" +
-            "                            <li><a href='/positions.html'>Positions</a></li>" +
-            "                            <li><a href='/contact.html'>Contact us</a></li>" +
-            "                        </ul>" +
-            "                    </li>" +
-            "                </ul>" +
-            "            </div>" +
-            "        </div>" +
-            "    </nav>"
+var html =  "" +
+"<nav class='navbar fixed-top navbar-dark bg-dark navbar-expand-lg' role='navigation'>" +
+"    <a class='navbar-brand' href='/index.html'>Myers Lab</a>" +
+"    <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>" +
+"        <span class='navbar-toggler-icon'></span>" +
+"    </button>" +
+"    <div class='collapse navbar-collapse' id='navbarSupportedContent'>" +
+"        <ul class='nav navbar-nav mr-auto'>" +
+"            <li class='nav-item'>" +
+"                <a class='nav-link' href='/research.html'>Research</a>" +
+"            </li>" +
+"            <li class='nav-item'>" +
+"                <a class='nav-link' href='/people.html'>People</a>" +
+"            </li>" +
+"            <li class='nav-item'>" +
+"                <a class='nav-link' href='/publication.html'>Publications</a>" +
+"            </li>" +
+"            <li class='nav-item'>" +
+"                <a class='nav-link' href='/software.html'>Software</a>" +
+"            </li>" +
+"            <li class='nav-item dropdown'>" +
+"                <a href='#' class='nav-link dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>" +
+"                    Other Pages" +
+"                </a>" +
+"                <div class='dropdown-menu' aria-labelledby='navbarDropdown'>" +
+"                    <a class='dropdown-item' href='/LabMemberArea/LabMemberArea.html'>Lab Member Area</a>" +
+"                    <a class='dropdown-item' href='/collaborators.html'>Collaborators</a><" +
+"                    <a class='dropdown-item' href='/positions.html'>Positions</a><" +
+"                    <a class='dropdown-item' href='/contact.html'>Contact us</a>" +
+"                </div>" +
+"            </li>" +
+"        </ul>" +
+"    </div>" +
+"</nav>"
 document.getElementById('nav').innerHTML = html;
